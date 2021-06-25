e174af4d7323a43d3ee3820fd7f0583afd7a43a2
Shawn Cicoria <github@cicoria.com>
2021-06-25 16:21:50 -0400

    meta data remove jpa dependency

--- clidemo/src/main/java/com/cicoria/samples/clidemo/repository/MessageRepository.java
+++ clidemo/src/main/java/com/cicoria/samples/clidemo/repository/MessageRepository.java
@@ -1,10 +1,10 @@
 package com.cicoria.samples.clidemo.repository;
 
-import org.springframework.data.repository.Repository;
+//import org.springframework.data.repository.Repository;
 import java.io.Serializable;
 
 //@org.springframework.stereotype.Repository
-public interface MessageRepository<T, ID extends Serializable> extends Repository<T, ID> {
+public interface MessageRepository<T, ID extends Serializable> { //extends Repository<T, ID> {
     T findOne(ID id);
     //T save(T entity);
     Iterable<T> findAll();
