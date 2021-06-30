56032326447ab0ef1ef05ffbdac1ab3d868e426c
Shawn Cicoria <github@cicoria.com>
2021-06-29 20:01:24 -0400

    refactor packages

--- clidemo/src/main/java/com/cicoria/samples/clidemo/CliDemoApplication.java
+++ clidemo/src/main/java/com/cicoria/samples/clidemo/CliDemoApplication.java
@@ -4,10 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
 
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication//(exclude={DataSourceAutoConfiguration.class})
 public class CliDemoApplication {
     private static final Logger log = LoggerFactory.getLogger(CliDemoApplication.class);
     public static void main(String[] args)
