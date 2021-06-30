eec8a8257c9a150c206ec58e1a202c80a50ce982
Shawn Cicoria <github@cicoria.com>
2021-06-29 22:41:43 -0400

    formatting

--- clidemo/src/main/java/com/cicoria/samples/clidemo/sender/MessageSender.java
+++ clidemo/src/main/java/com/cicoria/samples/clidemo/sender/MessageSender.java
@@ -3,26 +3,40 @@ package com.cicoria.samples.clidemo.sender;
 import com.cicoria.samples.clidemo.model.Message;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
 
 
 public class MessageSender {
     private static final Logger log = LoggerFactory.getLogger(MessageSender.class);
-    private String baseUrl;
+    private final String baseUrl;
+    private final WebClient webClient;
 
-    public MessageSender(String baseUrl) {
+    public MessageSender(String baseUrl)
+    {
         this.baseUrl = baseUrl;
+        this.webClient = WebClient
+                .builder()
+                .baseUrl(this.baseUrl)
+                .defaultHeaders(httpHeaders -> {
+                    httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+                    // TODO: conditional add authorization header name and key value
+                    httpHeaders.set("fish", "value");
+                })
+                .build();
+
     }
 
     public void Send(Message message) {
-        WebClient webClient = WebClient.create(this.baseUrl);
-        String url = baseUrl;
+
         try {
-            var resp = webClient.post().uri(url)
+            var resp = webClient.post().uri(baseUrl)
                     .bodyValue(message.getBody())
-                    .header("Content-Type", "application/json")
                     .retrieve().toBodilessEntity().block();
-            log.info(String.valueOf(resp.getStatusCode()));
+
+            if (null != resp)
+                log.info(String.valueOf(resp.getStatusCode()));
+
         } catch (Exception exp) {
             log.info(exp.getMessage());
         }
